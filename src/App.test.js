0ca109bc1dd33b23f23d6926f73aedab97be558c
christy313 <christycreativity@gmail.com>
2022-07-26 11:56:36 -0400

    fix typo

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import TodoItem from "./components/TodoItem";
 
 describe("<TodoItem />", () => {
-  const mockTodo = { id: 1, content: "test Todo", isDone: false };
+  const mockTodo = { id: 1, content: "test todo", isDone: false };
 
   it("should render a todo object", () => {
     const { debug } = render(<TodoItem todo={mockTodo} />);
