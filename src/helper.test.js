7b58a0620f5eab5cf15e27dc5fd7763e43409ba1
christy313 <christycreativity@gmail.com>
2022-07-28 21:19:23 -0400

    test getId fn

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import TodoItem from "./components/TodoItem";
+import getId from "./helper";
 
 describe("<TodoItem />", () => {
   const mockTodo = { id: 1, content: "test todo", isDone: false };
@@ -7,5 +8,11 @@ describe("<TodoItem />", () => {
   it("should render a todo object", () => {
     const { debug } = render(<TodoItem todo={mockTodo} />);
     debug();
+  });
+});
+
+describe("test id", () => {
+  it("should generate a id by time", () => {
+    expect(getId()).toBe(new Date().getTime());
   });
 });
