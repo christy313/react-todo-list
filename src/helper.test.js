3df3aa6418e6dd7206e2f2c04bcb5a56e0287cf2
christy313 <christycreativity@gmail.com>
2022-07-29 11:58:21 -0400

    delete debug method and pass TodoItem component test

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,18 +1,9 @@
-import { render } from "@testing-library/react";
-import TodoItem from "./components/TodoItem";
 import getId from "./helper";
 
-describe("<TodoItem />", () => {
-  const mockTodo = { id: 1, content: "test todo", isDone: false };
-
-  it("should render a todo object", () => {
-    const { debug } = render(<TodoItem todo={mockTodo} />);
-    debug();
-  });
-});
-
-describe("test id", () => {
-  it("should generate a id by time", () => {
+describe("getId", () => {
+  it("should generate an id by current time", () => {
     expect(getId()).toBe(new Date().getTime());
   });
 });
+const fixTime = new Date().getTime();
+console.log(fixTime);
